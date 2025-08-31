b309acfbe847f45947a178024936cceedadabd3d
ngil0001 <noahgilkey@gmail.com>
2025-08-31 13:48:25 -0400

    deleted greeting function and call

--- app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
+++ app/src/main/java/com/example/happybirthdayapp/MainActivity.kt
@@ -30,18 +30,12 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+
 
 @Preview(showBackground = true)
 @Composable
 fun BirthdayCardPreview() {
     HappyBirthdayAppTheme {
-        Greeting("Noah")
+
     }
 }
